677c39291ca40acc2c1b77ae81ded2e310c533b8
Kaique Damacena <kaiquead@live.com>
2025-07-07 23:04:26 -0300

    Creation of StringLocalizer file.

---
+++ swift-fundamentals/swift-fundamentals/Infrastructure/Strings/StringLocalizer.swift
@@ -0,0 +1,34 @@
+//
+//  StringLocalizer.swift
+//  swift-fundamentals
+//
+//  Created by Kaique Alves on 07/07/25.
+//
+
+import Foundation
+
+enum StringLocalizer {
+    
+    // MARK: - Home
+    
+    static var homeTitle: String {
+        self.localizeString("homeTitle")
+    }
+    
+    static var searchTitle: String {
+        self.localizeString("searchTitle")
+    }
+    
+    static var newsTitle: String {
+        self.localizeString("newsTitle")
+    }
+    
+    static var helloLabel: String {
+        self.localizeString("helloLabel")
+    }
+    
+    
+    static func localizeString(_ key: String) -> String {
+        return String(format: NSLocalizedString(key, comment: ""))
+    }
+}
