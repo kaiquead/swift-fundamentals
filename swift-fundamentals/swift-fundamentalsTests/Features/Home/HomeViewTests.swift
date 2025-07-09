be56a3cca0da23380e5a112749e2e0af11add8be
Kaique Damacena <kaiquead@live.com>
2025-07-09 20:41:50 -0300

    Correction in snapshot tests.

--- swift-fundamentals/swift-fundamentalsTests/Features/Home/HomeViewTests.swift
+++ swift-fundamentals/swift-fundamentalsTests/Features/Home/HomeViewTests.swift
@@ -11,16 +11,40 @@ import SnapshotTesting
 @testable import swift_fundamentals
 
 final class HomeViewTests: XCTestCase {
-
+    
     func testHomeViewSnapshot() {
         let view = HomeView()
         view.loadArticles(articles: [.mock(title: "Teste", description: "Teste", publishedAt: "2025-07-07T18:26:26Z")], totalArticles: 1)
-        
-        view.snp.makeConstraints { make in
-            make.width.equalTo(375)
-            make.height.equalTo(667)
+
+        let vc = UIViewController()
+        vc.view = view
+        vc.view.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+
+        let traits = UITraitCollection.current.modifyingTraits {
+            $0.userInterfaceStyle = .light
+            $0.displayScale = 2.0
         }
 
-        assertSnapshot(of: view, as: .image, record: false)
+        let config = ViewImageConfig(
+            size: CGSize(width: 375, height: 667),
+            traits: traits
+        )
+
+        assertSnapshot(of: vc, as: .image(on: config), record: false)
     }
 }
+
+
+
+
+//func testHomeViewSnapshot() {
+//    let view = HomeView()
+//    view.loadArticles(articles: [.mock(title: "Teste", description: "Teste", publishedAt: "2025-07-07T18:26:26Z")], totalArticles: 1)
+//    
+//    view.snp.makeConstraints { make in
+//        make.width.equalTo(375)
+//        make.height.equalTo(667)
+//    }
+//
+//    assertSnapshot(of: view, as: .image, record: false)
+//}
--- swift-fundamentals/swift-fundamentalsTests/Features/Login/LoginViewTests.swift
+++ swift-fundamentals/swift-fundamentalsTests/Features/Login/LoginViewTests.swift
@@ -15,11 +15,20 @@ final class LoginViewTests: XCTestCase {
     func testLoginViewSnapshot() {
         let view = LoginView()
         
-        view.snp.makeConstraints { make in
-            make.width.equalTo(393)
-            make.height.equalTo(852)
+        let vc = UIViewController()
+        vc.view = view
+        vc.view.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+        
+        let traits = UITraitCollection.current.modifyingTraits {
+            $0.userInterfaceStyle = .light
+            $0.displayScale = 2.0
         }
 
-        assertSnapshot(of: view, as: .image, record: false)
+        let config = ViewImageConfig(
+            size: CGSize(width: 375, height: 667),
+            traits: traits
+        )
+
+        assertSnapshot(of: vc, as: .image(on: config), record: false)
     }
 }
