7bd3b3824d5663d561b1a156a234ef95780fb89c
Kaique Damacena <kaiquead@live.com>
2025-07-09 00:08:19 -0300

    Testing error in pipeline.

--- swift-fundamentals/swift-fundamentals/Features/Home/HomeViewController.swift
+++ swift-fundamentals/swift-fundamentals/Features/Home/HomeViewController.swift
@@ -44,6 +44,8 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        errorTest
+        
         DispatchQueue.main.async {
             guard let view = self.view as? HomeView else { return }
             view.delegate = self
