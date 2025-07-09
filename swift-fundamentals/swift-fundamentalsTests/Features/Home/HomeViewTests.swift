e32bec4e804b02bc0b07a3f568b27519be57d2ed
Kaique Damacena <kaiquead@live.com>
2025-07-08 22:37:19 -0300

    Added Snapshot test for HomeView.

---
+++ swift-fundamentals/swift-fundamentalsTests/Features/Home/HomeViewTests.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewTestes.swift
+//  swift-fundamentalsTests
+//
+//  Created by Kaique Alves on 08/07/25.
+//
+
+import XCTest
+import SnapKit
+import SnapshotTesting
+@testable import swift_fundamentals
+
+final class HomeViewTests: XCTestCase {
+
+    func testHomeViewSnapshot() {
+        let view = HomeView()
+        view.loadArticles(articles: [.mock(title: "Teste", description: "Teste", publishedAt: "2025-07-07T18:26:26Z")], totalArticles: 1)
+        
+        view.snp.makeConstraints { make in
+            make.width.equalTo(393)
+            make.height.equalTo(852)
+        }
+
+        assertSnapshot(of: view, as: .image, record: false)
+    }
+}
